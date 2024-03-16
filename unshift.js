75599a6ee178695d1eb431533818cced580c1398
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-16 19:05:45 -0300

    Primeiro exemplo do unshift

---
+++ unshift.js
@@ -0,0 +1,16 @@
+//Exemplo 1:
+
+// Um array chamado colors é declarado e inicializado com três elementos: "Azul", "Verde" e "Roxo".
+let colors = ["Azul", "Verde", "Roxo"];
+
+//O método unshift() é usado para adicionar os elementos "Marrom" e "Rosa" no início do array colors. É póssivel adicionar multiplos itens de uma vez na ordem que são escritos.
+colors.unshift("Marrom", "Rosa");
+
+console.log("-----Lista das cores-----");//mensagem inicial.
+
+//o array colors é exibido no console.
+console.log(colors);// [ 'Marrom', 'Rosa', 'Azul', 'Verde', 'Roxo' ]
+
+
+
+
