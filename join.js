8f0cdef40e0a73e4e7b80c4f3c0cb917d3a13121
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-16 19:09:19 -0300

    Primeiro exemplo do join

---
+++ join.js
@@ -0,0 +1,13 @@
+//Exemplo 1:
+
+//Um array chamado palavras é declarado e inicializado com cinco strings.
+let palavras = ['Viver', 'é', 'desenhar', 'sem', 'borracha'];
+
+console.log("A frase do dia é:")// mensagem inicial.
+
+//O método join() é usado para juntar todos os elementos do array palavras em uma única string. 
+//O argumento " "  é o separador que será utilizado entre os elementos. Utilizei o espaço em branco, cada elemento é separado por ele. 
+console.log(palavras.join(" "));
+
+
+
