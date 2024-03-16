e1fcb64fdb74caa48aa231421a5e8d3a8b365af0
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-16 18:49:02 -0300

    Primeiro exemplo do push

---
+++ push.js
@@ -0,0 +1,12 @@
+// Exemplo 1:
+
+// Um array chamado names é declarado e inicializado com 4 elementos: "Júlia", "Gabriel", "Mateus", "Fernanda".
+let names = ["Júlia", "Gabriel", "Mateus", "Fernanda"];
+
+//O método push adiciona um ou mais elementos ao final de um array e retorna o novo comprimento desse array. Adicionei dois: "Amanda" e "Moisés".
+names.push("Amanda");
+names.push("Moisés");
+
+console.log(names); //[ 'Júlia', 'Gabriel', 'Mateus', 'Fernanda', 'Amanda', 'Moisés' ] exibe a lista com os itens adicionados.
+
+
