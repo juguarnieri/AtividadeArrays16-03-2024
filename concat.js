47cb9dba9e77cff5464836993780b0d4b387094b
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-16 23:29:57 -0300

    Exemplo 2 do concat

--- concat.js
+++ concat.js
@@ -12,3 +12,23 @@ let ambos = clothers.concat(accessories);
 console.log("Lista de compras femininas do mês:");// mensagem inicial para mostrar as compras.
 console.log(ambos);//Cria uma nova lista chamada ambos, que contém todos os itens de vestuário e acessórios. Lista de compras femininas do mês:[ 'saia', 'camisa', 'calça', 'pulseira', 'anel', 'colar' ]
 
+
+//Exemplo 2:
+
+//Declarei dois arrays de objetos. O array children contém objetos com o nome de crianças, enquanto o array adults contém objetos com o nome de adultos.
+let children = [{ name: "Júlia" }, { name: "Anna" }];
+let adults = [{ name: "Antonia" }, { name: "Neusa" }];
+
+// O método concat() é usado combinar os arrays children e adults em um único array chamado todos. 
+//Cria uma matriz que contém os elementos dos dois arrays originais, mantendo a mesma ordem.
+let todos = children.concat(adults);
+
+//O método unshift()é usado para adicionar um novo objeto { name: "Amanda" } ao início do array todos. 
+todos.unshift({ name: "Amanda" });
+
+// Os arrays são impressos no console (children e adults), 
+console.log(children);//[ { name: 'Júlia' }, { name: 'Anna' } ]
+console.log(adults);//[ { name: 'Antonia' }, { name: 'Neusa' } ]
+
+// Mensagem informativa "Lista completa dos nomes:".
+console.log("Lista completa dos nomes:", todos);//Lista completa dos nomes: [ { name: 'Amanda' },{ name: 'Júlia' },{ name: 'Anna' }, {name: 'Antonia' },{ name: 'Neusa' } ]
