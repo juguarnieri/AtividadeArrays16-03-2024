abd1f7c542f1ac154b2f2886aefcaf63c244b9d5
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-16 23:23:41 -0300

    Exemplo 1 do sort

---
+++ sort.js
@@ -0,0 +1,13 @@
+//Exemplo 1:
+
+//Defini um array chamado names que contém uma lista de nomes.
+let names = ["Sabrina", "Letícia", "Joaquim", "Eduardo", "Arthur"];
+
+//Esse método sort () é usado para ordenar os elementos de um array em ordem alfabética. 
+names.sort(); 
+
+//Mensagem inicial para informar a ordem.
+//Exibe os nomes em ordem alfabética.
+console.log("Segue os nomes da turma em ordem alfabética:", names);//Segue os nomes da turma em ordem alfabética: [ 'Arthur', 'Eduardo', 'Joaquim', 'Letícia', 'Sabrina' ]
+
+
