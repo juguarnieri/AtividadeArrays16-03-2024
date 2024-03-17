c21848edff112f2df2df143fd5311fc95f7e084a
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-17 00:07:38 -0300

    Exemplo 2 do reverse

--- reverse.js
+++ reverse.js
@@ -11,5 +11,25 @@ console.log("Lista inversa dos carros vendidos na semana:")//mensagem que a list
 console.log(carros); // [ 'Picasso', 'Celta', 'Corsa', 'Meriva', 'Corola' ] lista inversa.
 
 
+//Exemplo 2:
 
+// Um array chamado numbers é declarado e inicializado com uma sequência de números de 1 a 6.
+let numbers = [1, 2, 3, 4, 5, 6];
+
+//Criei uma cópia do array numbers, revertendo e atribuindo à variável numbersInvertido. 
+//O método slice(0) é usado para criar uma cópia do array numbers. O índice 0 para o slice() significa que copiei todos os elementos do array numbers.
+// O método reverse() é usado para inverter a ordem dos elementos no array. Este método modifica o array original, e não cria uma cópia. 
+//A cópia do slice é invertida.
+let numbersInvertido = numbers.slice(0).reverse();
+
+console.log(numbers); //[ 1, 2, 3, 4, 5, 6] Sem modificação.
+
+console.log(numbersInvertido); // [ 6, 5, 4, 3, 2, 1 ] Números invertidos.
+
+//Criei outra cópia invertida do array original `numbers`, armazenado na variável `numbersInvertidos`. 
+//`numbers.reverse()`,  que inverte a ordem dos elementos no array `numbers`.  
+let numbersInvertidos = numbers.reverse();
+
+//Exibiu o primeiro elemento do array `numbers` após a segunda inversão. 
+console.log(numbers[0]); //6
 
