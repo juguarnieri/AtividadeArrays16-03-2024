2e6f7c1f9373f818cba8be2e2517508337c39fb8
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-17 08:40:09 -0300

    Correção do primeiro exemplo

--- shift.js
+++ shift.js
@@ -6,14 +6,14 @@ let colors = ["Rosa", "Branco", "Verde", "Amarelo", "Azul"];
 //Mensagem inicial: "Ordem das cores antes: " seguida pelo conteúdo do array colors no console. Ordem das cores antes: Rosa,Branco,Verde,Amarelo,Azul
 console.log("Ordem das cores antes: ", colors);
 
-//Este comando remove o primeiro elemento do array colors e atribui esse elemento removido à variável shifted.
-let primeiro  = colors.shift(); 
+//Este comando shift () remove o primeiro elemento do array colors e atribui esse elemento removido à variável primeiro.
+let first = colors.shift();
 
 //Mensagem final: "Ordem das cores depois: " seguida pelo conteúdo atualizado do array colors no console. O primeiro elemento ("Rosa") foi removido. Ordem das cores depois: Branco,Verde,Amarelo,Azul
-console.log("Ordem das cores depois: ", colors); 
+console.log("Ordem das cores depois: ", colors);
 
-// Mensagem "Elemento removido: " seguido pelo elemento que foi removido do array colors, na variável 'primeiro'. Item removido: Rosa.
-console.log("Item removido: " , primeiro); 
+// Mensagem "Elemento removido: " seguido pelo elemento que foi removido do array colors, na variável 'firts'. Item removido: Rosa.
+console.log("Item removido: ", first);
 
 
 //Exemplo 2:
@@ -29,6 +29,6 @@ let clubes = ["São Paulo", "Palmeiras", "Santos", "Corinthians", "Ponte Preta"]
 
 while ((i = clubes.shift()) !== undefined) {
 
-//executar os itens removidos (valor atual de i)
+    //executar os itens removidos (valor atual de i)
     console.log(i);
 }
