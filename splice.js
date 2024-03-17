250c034445490d46b62f67c65e5f0d7c7c577872
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-17 08:25:07 -0300

    Exemplo remover do Splice

--- splice.js
+++ splice.js
@@ -13,3 +13,21 @@ console.log("Adicionar cor disponível:", colors);//Adicionar cor disponível: [
 //O método splice(), está adicionando multiplos elementos ao array colors, começando pela posição 5. Está adicionando esses elementos após o elemento na posição 4 (índice 5). 
 colors.splice(5, 0, 'Vermelho', 'Amarelo', 'Cinza');
 console.log("Adicionar mais cores disponíveis no total:", colors); // Adicionar mais cores disponíveis no total: ['Verde', 'Preto','Branco', 'Rosa','Roxo','Vermelho','Amarelo','Cinza','Azul']. com mensagem mostrando todas as cores disponíveis.
+
+
+//Exemplo Remover SPLICE:
+
+// Inicializando o array animals com uma lista de animais.
+let animals = ["gato", "cachorro", "cavalo", "vaca", "pato", "galinha", "jacaré"];
+
+// Usando splice() para remover elementos do array animals.
+//Está sendo removido elementos a partir do índice 1 (o segundo elemento, "cachorro") e removendo 2 elementos no total ("cachorro" e "cavalo"). Esses elementos removidos são armazenados na variável removedAnimals.
+let removedAnimals = animals.splice(1, 2);
+
+//Exibe os animais restantes no array animals após a remoção.
+console.log("Animais restantes:", animals);// Animais restantes: [ 'gato', 'vaca', 'pato', 'galinha', 'jacaré' ]
+
+//Exibe os animais removidos, que foram armazenados na variável removedAnimals.
+console.log("Animais removidos:", removedAnimals);//Animais removidos: [ 'cachorro', 'cavalo' ]
+
+
