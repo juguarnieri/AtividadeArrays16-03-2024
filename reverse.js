d7faecaf8e6c16905507e178365d0a261cd9cfeb
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-17 00:06:07 -0300

    Exemplo 1 do reverse

---
+++ reverse.js
@@ -0,0 +1,15 @@
+//Exemplo 1:
+
+//Um array chamado carros é declarado e inicializado com uma lista de modelos de carros.
+let carros = ["Corola", "Meriva", "Corsa", "Celta", "Picasso"];
+
+//O método reverse() é usado para inverter a ordem dos elementos no array carros. 
+carros.reverse();
+
+console.log("Lista inversa dos carros vendidos na semana:")//mensagem que a lista de carros vendidos na semana inversa será executada.
+
+console.log(carros); // [ 'Picasso', 'Celta', 'Corsa', 'Meriva', 'Corola' ] lista inversa.
+
+
+
+
