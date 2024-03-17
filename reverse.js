9fe40879e2ff4083fe9a65f047bc5ec0a2d3a98a
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-17 08:58:11 -0300

    Alteração do array

--- reverse.js
+++ reverse.js
@@ -1,14 +1,14 @@
 //Exemplo 1:
 
 //Um array chamado carros é declarado e inicializado com uma lista de modelos de carros.
-let carros = ["Corola", "Meriva", "Corsa", "Celta", "Picasso"];
+let cars = ["Corola", "Meriva", "Corsa", "Celta", "Picasso"];
 
 //O método reverse() é usado para inverter a ordem dos elementos no array carros. 
-carros.reverse();
+cars.reverse();
 
 console.log("Lista inversa dos carros vendidos na semana:")//mensagem que a lista de carros vendidos na semana inversa será executada.
 
-console.log(carros); // [ 'Picasso', 'Celta', 'Corsa', 'Meriva', 'Corola' ] lista inversa.
+console.log(cars); // [ 'Picasso', 'Celta', 'Corsa', 'Meriva', 'Corola' ] lista inversa.
 
 
 //Exemplo 2:
