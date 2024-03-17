e48478d638a00c2b3090456a409de3b9fe8d69c3
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-17 08:19:49 -0300

    Segundo exemplo do every

--- every.js
+++ every.js
@@ -13,3 +13,16 @@ let menoresQueDez = numeros.every(numero => numero < 10);
 console.log("Todos os números divisivéis por 3?", divisiveisPorTres); // Saída: Todos os números divisivéis por 3? true 
 console.log("Todos os números são menores que 10?", menoresQueDez); // Saída: Todos os números são menores que 10? false
 
+
+//Exemplo 2:
+
+//Defini um array chamado all, que contém uma variedade de tipos de dados.
+let all = ["bicicleta", false, "beterraba", 5, true, 15, -4];
+
+//O método every() verifica se todos os elementos do array satisfazem a condição fornecida por uma função de teste, no caso, para verificar se todos os elementos são do tipo número.
+//elemento => typeof elemento === "number" é uma função de callback que verifica se o tipo de dado de cada elemento é igual a "number" , para isso é usado o operador typeof.
+let apenasNumbers = all.every(elemento => typeof elemento === "number");
+
+//Se todos os elementos do array all forem do tipo número, a variável apenasNumbers será true, caso contrário, será false.
+//Como o array all contém elementos de vários tipos de dados, incluindo strings, booleanos e número, a saída vai ser: false.
+console.log("Todos os elementos são numbers?", apenasNumbers); //Todos os elementos são numbers? false
