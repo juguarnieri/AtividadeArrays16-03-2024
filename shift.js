fb222500452e5185a884da305024fcd956c1f000
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-17 08:56:58 -0300

    Alteração do array

--- shift.js
+++ shift.js
@@ -19,7 +19,7 @@ console.log("Item removido: ", first);
 //Exemplo 2:
 
 ////  Um array chamado clubes é definido e inicializado com cinco elementos.
-let clubes = ["São Paulo", "Palmeiras", "Santos", "Corinthians", "Ponte Preta"];
+let clubs = ["São Paulo", "Palmeiras", "Santos", "Corinthians", "Ponte Preta"];
 
 //Um loop while continuará executando enquanto o resultado de clubes.shift() não for undefined. Cada item é removido com o método shift() e é atribuído à variável i, dentro do loop.
 //Undefined é usado após todos os itens serem removidos do array clubes e o loop While terminar.
@@ -27,7 +27,7 @@ let clubes = ["São Paulo", "Palmeiras", "Santos", "Corinthians", "Ponte Preta"]
 //O elemento removido é atribuído à variável i.
 //Exemplo: O primeiro elemento é removido e atribuído a i. O valor de i será "São Paulo", que é então escrito, isso continua até que todos os elementos sejam removidos (iteração).
 
-while ((i = clubes.shift()) !== undefined) {
+while ((i = clubs.shift()) !== undefined) {
 
     //executar os itens removidos (valor atual de i)
     console.log(i);
