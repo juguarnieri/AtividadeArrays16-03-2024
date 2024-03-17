11b5019da8b724d768a4556e6cb6a943c7de7715
Júlia Guarnieri <juliaguarnieri04@gmail.com>
2024-03-17 08:53:29 -0300

    Alteração do array

--- pop.js
+++ pop.js
@@ -1,15 +1,15 @@
 //Exemplo 1:
 
 //Um array chamado animals é declarado e inicializado com 4 elementos:"pato", "cobra", "morcego", "elefante".
-let animais = ["pato", "cobra", "morcego", "elefante"];
+let animals = ["pato", "cobra", "morcego", "elefante"];
 
-console.log(animais); //[ 'pato', 'cobra', 'morcego', 'elefante' ] mostra todos os elementos presentes.
+console.log(animals); //[ 'pato', 'cobra', 'morcego', 'elefante' ] mostra todos os elementos presentes.
 
 // O método pop() remove um item do array, sendo ele o último elemento do array. 
 //Não atribui valor a variável.
-animais.pop();
+animals.pop();
 
-console.log(animais); // [ 'pato', 'cobra', 'morcego' ] o último elemento foi removido.
+console.log("Lista correta dos animais:", animals); // Lista correta dos animais: [ 'pato', 'cobra', 'morcego' ] o último elemento foi removido.
 
 
 //Exemplo 2: 
